c1c02a5793d825af2cb08d6de7660a40b8dbf371
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-09-09 10:29:59 +0100

    all sell in tests passing with existing code and classes

--- src/test/kotlin/GildedRoseTest.kt
+++ src/test/kotlin/GildedRoseTest.kt
@@ -11,6 +11,30 @@ internal class GildedRoseTest {
         assertEquals("normal item sell in", app.items[0].sellIn, 9)
     }
 
+    @Test
+    fun testAgedBrieItemsSellInDate() {
+        val items = arrayOf<Item>(Item("Aged Brie", 10, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Aged Brie item sell in", app.items[0].sellIn, 9)
+    }
+
+    @Test
+    fun testBackStagePassItemsSellInDate() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 10, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Backstage Pass item sell in", app.items[0].sellIn, 9)
+    }
+
+    @Test
+    fun testSulfurasItemsSellInDate() {
+        val items = arrayOf<Item>(Item("Sulfuras, Hand of Ragnaros", 10, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Sulfuras item sell in", app.items[0].sellIn, 10)
+    }
+
 
 
 }
